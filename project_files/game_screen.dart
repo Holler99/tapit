278638afd291869719ab199e4d736ed07e459cff
Zane Camp <zanecamp@yahoo.com>
2023-04-19 17:15:10 -0500

    Added the game screen; removed firebase from main

--- project_files/game_screen.dart
+++ project_files/game_screen.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tap-it Game',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tap-it Game'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'High Score: 0',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Tap-it!',
+                style: TextStyle(fontSize: 36),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
