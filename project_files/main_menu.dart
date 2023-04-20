444dbc6c04dc54a6eb7857cd94fa5ebbeb8f7772
Peyton <pjbowden53@gmail.com>
2023-04-19 20:12:46 -0500

    Basis for the main menu

--- project_files/main_menu.dart
+++ project_files/main_menu.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tap-it Menu',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tap-it Menu'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'TAP IT',
+              style: TextStyle(fontSize: 50),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'PLAY',
+                style: TextStyle(fontSize: 36),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
