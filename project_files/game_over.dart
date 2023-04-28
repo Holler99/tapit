50b6842047de9a3e859151d75f39f091e5fa5607
JTAdams <jtadams6501@gmail.com>
2023-04-28 14:19:37 -0500

    Game over ui

--- project_files/game_over.dart
+++ project_files/game_over.dart
@@ -1,1 +1,38 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tap-it Game',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Tap-it Game'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Your Score!: 0',
+              style: TextStyle(fontSize: 40),
+            ),
+
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+              // Navigate back to the first screen by popping the current route
+              // off the stack.
+              Navigator.pop(context);
+
+},
+              child: const Text(
+                'Back To Menu',
+                style: TextStyle(fontSize: 36),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
