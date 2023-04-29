b42f581f538019ccf82fcbc184ec4be9b1fa9b1b
JTAdams <jtadams6501@gmail.com>
2023-04-29 01:12:53 -0500

    Hopefully yall see this one?
    Game Over UI and Nav

--- project_files/game_over.dart
+++ project_files/game_over.dart
@@ -3,36 +3,52 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Tap-it Game',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tap-it Game'),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Your Score!: 0',
-              style: TextStyle(fontSize: 40),
-            ),
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Game Over',
+     home: Scaffold(
 
-            const SizedBox(height: 20),
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: <Widget>[
+
+              const Text(
+              'GAME OVER!',
+              style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+            ),
+             const Text(
+               'Your Score!: 0',
+               style: TextStyle(fontSize: 40),
+             ),
+               const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-              // Navigate back to the first screen by popping the current route
-              // off the stack.
-              Navigator.pop(context);
-
-},
+                // Navigate back to the first screen by popping the current route
+                // off the stack.
+                Navigator.pop(context);
+              },
               child: const Text(
                 'Back To Menu',
+                style: TextStyle(fontSize: 36),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                // Close the app
+               // SystemNavigator.pop();
+              },
+              child: const Text(
+                'Exit',
                 style: TextStyle(fontSize: 36),
               ),
             ),
           ],
         ),
       ),
-    );
+    ),
+  );
+}
+}
